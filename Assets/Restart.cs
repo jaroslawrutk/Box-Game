3991c08a02f9d97f0ed5ebc7fb86bfd0ce83b62f
jaroslawrutk <choresize@gmail.com>
2018-11-19 18:57:55 +0100

    Add Start and Ending scene

---
+++ Assets/Restart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+    Button startGame;
+    private void Start()
+    {
+        startGame = GameObject.Find("Reset").GetComponent<Button>();
+        startGame.onClick.AddListener(() => Startuje());
+    }
+    public void Startuje()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
